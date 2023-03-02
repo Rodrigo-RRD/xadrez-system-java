d35bc4f0966861c1d9fe8299b63c3063679f6f83
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-02 17:39:00 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,9 +1,10 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
+
 import chess.pieces.King;
 import chess.pieces.Rook;
+
 
 public class ChessMatch {
     //Coração do sistema
@@ -31,10 +32,18 @@ private void placeNewPiece(char column, int row, ChessPiece piece){
 }
 
 private void initialSetup(){
-        placeNewPiece('b',6,new Rook(board, Color.WITHE));
-        placeNewPiece('e',8,new King(board, Color.BLACK));
-        placeNewPiece('e',1,new King(board, Color.WITHE));
+    placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+    placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+    placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+    placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+    placeNewPiece('d', 1, new King(board, Color.WHITE));
 
-
+    placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+    placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+    placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+    placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+    placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+    placeNewPiece('d', 8, new King(board, Color.BLACK));
 }
 }
+
--- src/chess/Color.java
+++ src/chess/Color.java
@@ -3,6 +3,6 @@ package chess;
 public enum Color {
 
     BLACK,
-    WITHE;
+    WHITE;
 
 }
