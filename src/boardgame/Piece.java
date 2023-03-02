5aa9c5c4b8fbc6dcfdc367c7d4d41dd2272c2601
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-02 14:29:52 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    protected Board board;
+
+
+    public Piece(Board board) { //para criar uma peça tenho que informar o board
+        this.board = board;
+        position = null;    //o java ja deixa nullo
+    }
+
+    protected Board getBoard() { //somente classse dentro do mesmo pacote e subclasses vão poder acessar o taboleiro de uma peça
+        return board;
+    }
+}
