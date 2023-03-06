1aafa79ed1830a92e8e3f0f8baa3791bce188aed
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-06 15:19:47 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,14 +31,19 @@ public class Program {
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
 
-
                 System.out.println();
                 System.out.println("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
                 if (capturedPiece !=null){
                     captured.add(capturedPiece);
+                }
+                if (chessMatch.getPromoted() != null){
+                    System.out.printf("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             }catch (ChessException e){
                 System.out.println(e.getMessage());
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,6 +6,7 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +23,7 @@ public class ChessMatch {
     private boolean check;
     private boolean checkMate;
     private ChessPiece enPassanVulnerable;
+    private ChessPiece promoted;
 
     public ChessMatch(){
         //é nessa classe que vai informar que vai ser 8 x 8 o tabuleiro
@@ -46,6 +48,9 @@ public class ChessMatch {
     public ChessPiece getEnPassanVulnerable(){
         return enPassanVulnerable;
     }
+    public ChessPiece getPromoted(){
+        return promoted;
+    }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0; i < board.getRows();i++){
@@ -67,12 +72,22 @@ public class ChessMatch {
     validateSourcePosition(source); //validar a posição de origem
     validateTargetPosition(source,target);  //validar a posição de destino
     Piece capturedPiece = makeMove(source, target);
+
     if (testCheck(currentPlayer)){
         undoMove(source,target,capturedPiece);
         throw new ChessException("You can´t put yourself in check");
     }
 
     ChessPiece movePiece = (ChessPiece)board.piece(target);
+
+    //#Specialmove promotion
+        promoted = null;
+        if (movePiece instanceof Paw){
+            if ((movePiece.getColor() == Color.WHITE && target.getRow() == 0) || (movePiece.getColor() == Color.BLACK && target.getRow() == 7 )){
+        promoted = (ChessPiece)board.piece(target);
+        promoted = replacePromotedPiece("Q");
+            }
+        }
 
     check = (testCheck(opponent(currentPlayer))) ? true : false;
 
@@ -92,6 +107,29 @@ public class ChessMatch {
     return (ChessPiece)capturedPiece;
 }
 
+    public  ChessPiece replacePromotedPiece(String type){
+        if (promoted == null){
+            throw new IllegalStateException("There is no piece to be promoted");
+        }
+        if (!type.equals("B") &&!type.equals("N") &&!type.equals("R") &&!type.equals("Q")){
+        throw new InvalidParameterException("Invaid type for promotion");
+    }
+    Position pos = promoted.getChessPosition().toPosition();
+        Piece p = board.removePiece(pos);
+        piecesOnTheBoard.remove(p);
+
+    ChessPiece newPiece = newPiece(type, promoted.getColor());
+        board.placePiece(newPiece, pos);
+        piecesOnTheBoard.add(newPiece);
+
+        return newPiece;
+}
+private ChessPiece newPiece(String type, Color color){
+        if (type.equals("B")) return new Bishop(board, color);
+        if (type.equals("N")) return new Knight(board, color);
+        if (type.equals("Q")) return new Queen(board, color);
+        return new Rook(board, color);
+}
 //Tira da origem e coloca no destino
     private Piece makeMove(Position source, Position target){
        ChessPiece p = (ChessPiece) board.removePiece(source);
