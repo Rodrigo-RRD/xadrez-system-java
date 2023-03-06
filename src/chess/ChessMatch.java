d08ab574cbbb55478ad9b69acd54d9ea8ec286d9
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-06 13:29:04 -0300

    Bishop

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,6 +4,7 @@ import boardgame.Board;
 
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Paw;
 import chess.pieces.Rook;
@@ -188,7 +189,9 @@ if (testCheckMate(opponent(currentPlayer))){
 
     private void initialSetup(){
     placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+     placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('e', 1, new King(board, Color.WHITE));
+     placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
     placeNewPiece('h', 1, new Rook(board, Color.WHITE));
     placeNewPiece('a', 2, new Paw(board, Color.WHITE));
     placeNewPiece('b', 2, new Paw(board, Color.WHITE));
@@ -200,7 +203,9 @@ if (testCheckMate(opponent(currentPlayer))){
     placeNewPiece('h', 2, new Paw(board, Color.WHITE));
 
     placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+     placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
     placeNewPiece('e', 8, new King(board, Color.BLACK));
+     placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
     placeNewPiece('h', 8, new Rook(board, Color.BLACK));
     placeNewPiece('a', 7, new Paw(board, Color.BLACK));
     placeNewPiece('b', 7, new Paw(board, Color.BLACK));
